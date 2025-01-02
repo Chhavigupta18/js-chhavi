240c828a12168090c4ae3a86d44315323c0cbfce
CHHAVI GUPTA <guptachhavi381@gmail.com>
2025-01-02 06:37:22 +0000

    Conversion

---
+++ 01_basics/03ConversionOperatior.js
@@ -0,0 +1,54 @@
+//let score=33
+//or
+// let score="33abc"
+//or
+// let score=null
+//or
+// let score=undefined
+//or
+// let score=true
+// or 
+let score="Chhavi"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber)
+
+
+//Conversions
+// "33"=> 33
+//"33abc"=NaN (not a number)
+// null=0
+//undefined=>
+    //true=>1; false=>0
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//CONVERSION OF DATATYPES INTO BOOLEAN
+
+
+// let isLoggedIn= 1
+// let isLoggedIn=0
+// let isLoggedIn=""
+let isLoggedIn="Chhavi"
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//CONVERSION INTO STRING
+
+// let a=33
+// let a="33"
+// let a=true
+// let a=false
+// let a= null
+let a=undefined
+let string=String(a)
+console.log(typeof string);
+
+
+
