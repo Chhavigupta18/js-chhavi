6f1d9a5b354daa5b1c0403877b2834a62a4ca291
CHHAVI GUPTA <guptachhavi381@gmail.com>
2025-01-02 05:53:25 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")
+// alert(3+3) we are using node.js not browser
+console.log(3+
+    3
+) //code reabaility should be high
+ 
+let namee="Chhavi"
+let age=18
+let isLoggedIn= false
+let state=""
+
+//Following are the data types
+//primitive data types
+//number=>2 to the power of 53
+// bigint
+//string=""
+//boolean =>  true or false
+//null=> standalone value (it is an empty )
+//undefined=> which value has not assigned
+//symbol=> unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof namee);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null); //output= object
+console.log(typeof undefined); //output = undefined
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail= "chhavig155"
+var accontPassword= "123450"
+accountCity= "Delhi"
+let accountState;
+
+//accountId=2 // not allowed
+console.log(accountId);
+accountEmail= "hechsgf@.com"
+accontPassword="121214"
+accountCity="jaipur"
+
+/*prefer not to use var because of issues in block scope and functional scope*/
+
+
+console.table([accontPassword,accountEmail,accountId,accountState,accountCity])
+
+
+
