ed24fd512abda7638f032603d36a95e3f11e248d
CHHAVI GUPTA <guptachhavi381@gmail.com>
2025-01-03 18:45:10 +0000

    dates

---
+++ 01_basics/07Dates.js
@@ -0,0 +1,20 @@
+
+
+let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+// let mycreatedate= new Date(2023,0,23)
+// let mycreatedate= new Date(2023,0,23,5,3)
+let mycreatedate= new Date("2023-01-14")
+console.log(mycreatedate.toLocaleString());
+
+let myTimeStamp= Date.now()
+console.log(myTimeStamp);
+// console.log(myDate);
+// console.log(myDate);
+// console.log(myDate);
+
+
+
