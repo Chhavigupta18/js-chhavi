1d2ef2e05d1c018518b7559e5c0b7dc83dc57ae4
CHHAVI GUPTA <guptachhavi381@gmail.com>
2025-01-03 09:14:30 +0000

    strings

---
+++ 01_basics/06strings.js
@@ -0,0 +1,37 @@
+const name="chhavi gupta"
+const repocount=50
+console.log(name+repocount+"Value");
+console.log(`Hello my name i ${name} and my repo count is ${50}`);
+ const gameName= new String('Hitesh-hc-com')
+ console.log(gameName[0]);
+ console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2))
+ console.log(gameName.indexOf("a"));
+ const newString=gameName.substring(0,4)
+ console.log(newString);
+
+ const anotherString= gameName.slice(-8,4)
+ console.log(anotherString);
+
+ const Str1= "         hitesh        "
+ console.log(Str1);
+ console.log(Str1.trim());
+ const url="https://hitesh.comhitesh%20choudhary"
+ console.log(url.replace('%20','-'))
+ console.log(url.includes('hitesh'))
+ console.log(gameName.split('-'))
+//////////////////////////////////////////////////////use of .at////////////////////////////////////////////////////////////
+ const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+let index = 5;
+
+console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+// Expected output: "An index of 5 returns the character u"
+
+
+ 
+ 
+ 
+ 
