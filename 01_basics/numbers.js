b3a4652a5c69ccfc9ee643f14b6ac9a3450f5e87
CHHAVI GUPTA <guptachhavi381@gmail.com>
2025-01-03 16:51:14 +0000

    number and maths

---
+++ 01_basics/numbers.js
@@ -0,0 +1,47 @@
+const  score=400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+
+let no=123.8966
+no=23.8966
+console.log(no.toPrecision(3));
+console.log(no.toPrecision(3));
+console.log(no.toPrecision(4));
+console.log(no.toPrecision(5));
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-In'));
+
+//++++++++++++++++++++++++++++++++++  MATHS  +++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.25));
+// console.log(Math.ceil(4.25));
+// console.log(Math.floor(4.23));
+// console.log(Math.min(4.23,2,3,4,5));
+// console.log(Math.max(4.23,2,3,4,5));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1); 
+
+const min=10
+const max=20
+console.log((Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
