39936675275a5d75800145653d1cc30730a93602
CHHAVI GUPTA <guptachhavi381@gmail.com>
2025-01-18 04:23:28 +0000

    data

--- 01_basics/03ConversionOperatior.js
+++ 01_basics/03ConversionOperatior.js
@@ -1,4 +1,4 @@
-//let score=33
+// let score=33
 //or
 // let score="33abc"
 //or
@@ -8,23 +8,26 @@
 //or
 // let score=true
 // or 
-let score = "Chhavi"
 
-console.log(typeof score);
-console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let score = "Chhavi"
+// console.log(typeof score);
+// console.log(typeof (score));
 
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
 
 
 //Conversions
+
+
 // "33"=> 33
-//"33abc"=NaN (not a number)
+// "33abc"=NaN (not a number)
 // null=0
-//undefined=>
-//true=>1; false=>0
+// undefined=> nan
+// true=>1; false=>0
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -34,9 +37,11 @@ console.log(valueInNumber)
 // let isLoggedIn= 1
 // let isLoggedIn=0
 // let isLoggedIn=""
-let isLoggedIn = "Chhavi"
-let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+
+
+// let isLoggedIn = "Chhavi"
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
 
 /////////////////////////////////////////////////////////////////////////////////////////
 //CONVERSION INTO STRING
@@ -46,39 +51,44 @@ console.log(booleanIsloggedIn);
 // let a=true
 // let a=false
 // let a= null
-let a = undefined
-let string = String(a)
-console.log(typeof string);
+
+
+
+// let a = undefined
+// let string = String(a)
+// console.log(typeof string);
+// console.log(string);
+
 
 ///***********************************OPERATIONS******************************************************* */
 
 
-let value = 3
-let negvalue = -value
-console.log(negvalue);
-console.log(2 + 20)
-console.log(2 - 2)
-console.log(2 * 2)
-console.log(2 ** 3);
-console.log(2 / 3);
-console.log(2 % 3)
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+// console.log(2 + 20)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3)
 
-let str1="Hi"
-let str2= " Chhavi"
-let str3= str1+str2    
-console.log(str3)       //output=Hi Chhavi
-console.log(1+"2");     //output=12
-console.log("1"+2);     //output=12
-console.log("1"+"2");   //output=12
-console.log("1"+2+2);  //output=122
-console.log("1"+(2+2)); // we use paranthesis and got diff ans from the above variable
-console.log(2+2+"1");  //output =41
-console.log(3+4*5%3);
-console.log(((3+4)*5)%3);
-console.log(+true);
-console.log(+"");
-let num1,num2,num3
-num1=num2=num3=8+8
+// let str1="Hi"
+// let str2= " Chhavi"
+// let str3= str1+str2    
+// console.log(str3)       //output=Hi Chhavi
+// console.log(1+"2");     //output=12
+// console.log("1"+2);     //output=12
+// console.log("1"+"2");   //output=12
+// console.log("1"+2+2);  //output=122
+// console.log("1"+(2+2)); // we use paranthesis and got diff ans from the above variable
+// console.log(2+2+"1");  //output =41
+// console.log(3+4*5%3);
+// console.log(((3+4)*5)%3);
+// console.log(+true);
+// console.log(+"");
+// let num1,num2,num3
+// num1=num2=num3=8+8
 let gamecounter=100
 gamecounter++
 console.log(gamecounter)
--- 01_basics/04Comparisons.js
+++ 01_basics/04Comparisons.js
@@ -1,26 +1,30 @@
-// console.log(2>1);
-// console.log(2>=1);
-// console.log(2<1);
-// console.log(2==1);
-// console.log(2!=1);
-console.log("2">1);
-console.log("02">1);
+// console.log(2>1);          // true
+// console.log(2>=1);          // true
+// console.log(2<1);           // false
+// console.log(2==1);         // false
+// console.log(2!=1);          // true
+// console.log("2">1);          // true
+// console.log("0">1);          // false
+// console.log("02">1);        // true
 
-console.log(null>0); //false
-console.log(null==0); //false
-console.log(null>=0); //true
+// console.log(null>0); //false
+// console.log(null==0); //false
+// console.log(null>=0); //true
 
 /*the reason is that an equity check == and comparisons ><= <= work differently. Comparisons convert 
 null to a number, treating it as 0. Thats why (3)null>=0 is true and (1)null>0 is false*/
- console.log(undefined==0);
- console.log(undefined >0);
- console.log(undefined>=0);
- console.log(undefined< 0);
+
+
+//  console.log(undefined==0);    // fALSE
+//  console.log(undefined >0);     // false
+//  console.log(undefined>=0);      //false
+//  console.log(undefined< 0);       // false
   
 
  // === (strict check)
- console.log("2"===2);
- console.log("2"=2);
+
+//  console.log("2"===2);
+//  console.log("2"==2);
  
  
  
